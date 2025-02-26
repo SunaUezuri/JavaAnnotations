810a98ed6ba4f45bc9af2f27228bb06985b21199
SunaUezuri <wesley.jane.santos@gmail.com>
2025-02-25 21:18:03 -0300

    Iniciando a implementação das anotações.

--- src/main/java/org/example/annotation/Coluna.java
+++ src/main/java/org/example/annotation/Coluna.java
@@ -1,4 +1,17 @@
 package org.example.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//Define até quando a anotação estará presente
+@Retention(RetentionPolicy.RUNTIME)
+//Define onde pode ser utilizada a anotação(atributos)
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface Coluna {
+
+    String nome();
+    int tamanho() default 0;
+    boolean obrigatorio() default false;
 }
--- src/main/java/org/example/dao/JpaDao.java
+++ src/main/java/org/example/dao/JpaDao.java
@@ -1,4 +1,14 @@
 package org.example.dao;
 
+
+import org.example.annotation.Tabela;
+
 public class JpaDao {
+
+    public void pesquisar(Object obj) {
+        //Recuperar a anotação @Tabela
+        Tabela anotacao = obj.getClass().getAnnotation(Tabela.class);
+        System.out.println("SELECT * FROM " + anotacao.nome());
+    }
+
 }
